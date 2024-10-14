ac71303c873822ccfc4e63559455533459b1b84b
FATUNBI ALIAMEEN (OLAMILEKAN) <adebukolaolamilekan123@gmail.com>
2024-10-14 15:57:47 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,44 @@
+// tailwind.config.js
 module.exports = {
-    // ...
-    plugins: [
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+        slideUp: {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        slideDown: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(100%)' },
+        },
+        slideLeft: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        slideRight: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+        fadeOut: 'fadeOut 1s ease-in-out',
+        slideUp: 'slideUp 1s ease-in-out',
+        slideDown: 'slideDown 1s ease-in-out',
+        slideLeft: 'slideLeft 1s ease-in-out',
+        slideRight: 'slideRight 1s ease-in-out',
+      },
+    },
+  },
+  plugins: [
       require('@tailwindcss/typography'),
       // other plugins...
     ],
