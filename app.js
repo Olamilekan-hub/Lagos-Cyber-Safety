2691ff7f505ffc9003b95ac3ce0d5e80b83b577a
FATUNBI ALIAMEEN (OLAMILEKAN) <adebukolaolamilekan123@gmail.com>
2024-10-17 02:03:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,16 +31,16 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
  // Close menu when any button in the overlay is clicked
-    const menuButtons = menuOverlay.querySelectorAll('a'); // Assuming buttons inside overlay
-    menuButtons.forEach(a => {
-        a.addEventListener('click', () => {
-            navBarOpen = false;
-            menuOverlay.classList.add('hidden');
-            menuIcon.innerHTML = `
-                <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
-                </svg>
-            `;
-        });
-    });
-});
+  //  const menuButtons = menuOverlay.querySelectorAll('a'); // Assuming buttons inside overlay
+ //   menuButtons.forEach(a => {
+//        a.addEventListener('click', () => {
+//            navBarOpen = false;
+//            menuOverlay.classList.add('hidden');
+//            menuIcon.innerHTML = `
+ //               <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+ //                   <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+ //               </svg>
+//            `;
+//        });
+  //  });
+// });
